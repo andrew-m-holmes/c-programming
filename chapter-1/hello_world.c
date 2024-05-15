475d5760f94fb58bcfe45c4c7fbedcd70d6d9958
Andrew Holmes <andrewholmes011002@gmail.com>
2024-05-15 16:21:16 -0400

    format

--- chapter-1/hello_world.c
+++ chapter-1/hello_world.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    printf("hello world\n");
+  printf("hello world\n");
 
-    printf("hello ");
-    printf("\"world!\"");
-    printf("\n");
-    return 0;
+  printf("hello ");
+  printf("\"world!\"");
+  printf("\n");
+
+  printf(
+      "what happens when i use an invalid escape character such as \e"); // generates
+                                                                         // %
+  return 0;
 }

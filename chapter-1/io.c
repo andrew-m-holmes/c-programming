830dafd0e3ae40e1a810684ba8e08babb0611916
Andrew Holmes <andrewholmes011002@gmail.com>
2024-05-16 15:13:10 -0400

    couting characters

--- chapter-1/io.c
+++ chapter-1/io.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
-int main() {
+void printchar() {
   int c;
   printf("The end of file charactetr is %d\n", EOF);
   while ((c = getchar()) != EOF) {
     putchar(c);
   }
 }
+
+void countchar() {
+  int c;
+  long count;
+  for (count = 1; getchar() != EOF; ++count) {
+    printf("Character count: %ld\n", count);
+  }
+}
+
+int main() { countchar(); }

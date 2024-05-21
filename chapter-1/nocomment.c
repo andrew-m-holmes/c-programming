1e37af86e3d709e448e7cf5562aa70a425fd480c
Andrew Holmes <andrewholmes011002@gmail.com>
2024-05-21 17:48:49 -0400

    program that cleans comments

--- chapter-1/nocomment.c
+++ chapter-1/nocomment.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-#define BUFFER 1000
+#define MAXLEN 1000
+
+int getindex(char line[]);
+void cleansl(char line[], int start);
+int cleanml(char line[], int start);
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
@@ -14,9 +18,55 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "Error: could not open %s", argv[1]);
   }
 
-  char buffer[BUFFER];
-  while (fgets(buffer, sizeof(buffer), file) != NULL) {
-    printf("%s", buffer);
+  // some random comment
+  char line[MAXLEN];
+  int inside, i;
+  inside = i = 0;
+
+  while (fgets(line, sizeof(line), file) != NULL) { // some random comment again
+    if (inside) {
+      inside = cleanml(line, 0);
+    } else if ((i = getindex(line)) != -1) {
+      if (line[i] == '/' && line[i + 1] == '/') {
+        cleansl(line, i);
+      } else { /*inline comment test */
+        inside = cleanml(line, i);
+      }
+    }
+    printf("%s", line); /*
+   multiline test */
   }
+
   return 0;
+}
+
+int getindex(char line[]) {
+  for (int i = 0; i < MAXLEN - 1; ++i) {
+    if (line[i] == '/' && (line[i + 1] == '/' || line[i + 1] == '*')) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+void cleansl(char line[], int start) {
+  int i = start;
+  while (line[i] != '\n' && line[i] != '\0') {
+    line[i++] = '\b';
+  }
+}
+
+int cleanml(char line[], int start) {
+  for (int i = start; i < MAXLEN - 1; ++i) {
+    if (line[i] == '*' && line[i + 1] == '/') {
+      line[i] = line[i + 1] = '\b';
+      return 0;
+    }
+
+    if (line[i] == '\n') {
+      break;
+    }
+    line[i] = '\b';
+  }
+  return 1;
 }

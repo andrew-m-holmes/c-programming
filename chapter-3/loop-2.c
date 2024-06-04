9ed6e0e0da74fbabe3c4208207f61c0cf616855e
Andrew Holmes <andrewholmes011002@gmail.com>
2024-06-03 21:09:12 -0400

    do while

--- chapter-3/loop-2.c
+++ chapter-3/loop-2.c
@@ -9,18 +9,31 @@ void reverse(char s[]);
 int main() {
   char s[MAXLEN];
   printf("%s\n", itoa(5, s));
+  printf("%s\n", itoa(20, s));
+  printf("%s\n", itoa(321, s));
+  printf("%s\n", itoa(2892, s));
+  printf("%s\n", itoa(-1e3, s));
   return 0;
 }
 
 char *itoa(int n, char s[]) {
-  int i, r;
-  i = 0;
+  int i, r, neg;
+  i = neg = 0;
+
+  if (n < 0) {
+    neg = 1;
+    n *= -1;
+  }
 
   do {
     r = n % 10;
     n /= 10;
     s[i++] = '0' + r;
   } while (n);
+
+  if (neg) {
+    s[i++] = '-';
+  }
 
   s[i] = '\0';
   reverse(s);

ab2d03853e4924dc5433c6fde0ff54b6d50de1b9
Andrew Holmes <andrewholmes011002@gmail.com>
2024-06-10 20:03:58 -0400

    break and continue

--- chapter-3/loop-2.c
+++ chapter-3/loop-2.c
@@ -5,6 +5,9 @@
 
 char *itoa(int n, char s[]);
 void reverse(char s[]);
+void trim(char s[]);
+int *arrabs(int arr[], unsigned int n);
+void printarr(int arr[], unsigned int n);
 
 int main() {
   char s[MAXLEN];
@@ -13,6 +16,17 @@ int main() {
   printf("%s\n", itoa(321, s));
   printf("%s\n", itoa(2892, s));
   printf("%s\n", itoa(-1e3, s));
+
+  char t[] =
+      "Hello, my name is Andrew\n\t How are you doing today?\t\t\n\n      ";
+  printf("%s\n", t);
+  trim(t);
+  printf("%s\n", t);
+
+  int arr[] = {-2, -1, 3, 4, -5, 6, 4};
+  printarr(arr, 7);
+  printarr(arrabs(arr, 7), 7);
+  printarr(arrabs(arr, 7), 7);
   return 0;
 }
 
@@ -48,4 +62,36 @@ void reverse(char s[]) {
     s[i] = s[j];
     s[j] = c;
   }
+}
+
+void trim(char s[]) {
+  int i, n;
+  n = strlen(s);
+  for (i = n - 1; i >= 0; --i) {
+    if (s[i] != ' ' && s[i] != '\n' && s[i] != '\t' && s[i] != '\0') {
+      break;
+    }
+  }
+  s[i + 1] = '\0';
+}
+
+int *arrabs(int arr[], unsigned int n) {
+  for (int i = 0; i < n; ++i) {
+    if (arr[i] >= 0) {
+      continue;
+    }
+    arr[i] *= -1;
+  }
+  return arr;
+}
+
+void printarr(int arr[], unsigned int n) {
+  putchar('[');
+  for (int i = 0; i < n; ++i) {
+    printf("%d", arr[i]);
+    if (i < n - 1) {
+      putchar(' ');
+    }
+  }
+  printf("]\n");
 }
